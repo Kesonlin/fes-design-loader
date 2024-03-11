f58ad4754477ebfbb03543c960c7bffbf0852ab5
kesonlin <kesonlin@webank.com>
2024-03-11 11:38:36 +0800

    feat: 打包配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,22 @@ module.exports = {
     env: {
         'browser': true,
         'amd': true,
-        'node': true
+        'node': true,
+        'es6': true
     },
     extends: [
         'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
     ],
+    parserOptions: {
+        sourceType: 'module'
+    },
     rules: {
         'quotes': ['error', 'single'],
         'no-extra-semi': 2
     },
+    plugins: [
+        '@typescript-eslint'
+    ]
    
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
+const path = require('path')
+
 module.exports = {
-    
+    mode: 'production',
+    entry: path.resolve(__dirname, './src/index.ts'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        libraryTarget: 'umd',
+        library: 'fes-design-loader'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ['ts-loader'],
+                exclude: '/node_modules'
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.ts']
+    }
 }
